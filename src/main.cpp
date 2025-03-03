3b703ac3925c491daa5634354c80411b4316a6f5
sangeetsourav <sangeetsourav@gmail.com>
2025-03-03 12:43:10 +0530

    Add REPL #1

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+  while(true)
+  {
+      std::cout << "$ ";
+      std::string input;
+      std::getline(std::cin, input);
+      std::cout << input << ": command not found" << std::endl;
+  }
 }
