89d36424a6e5a96c8b8fb9390aa06495fd692c6e
Sunderroy, Sangeet <sangeet.sunderroy@ge.com>
2025-05-27 19:24:43 +0530

    Revert "Removed map of earlier command paths"
    This reverts commit 5baaa05ddae45a483fc946ff86836decd7510804.

--- src/util.cpp
+++ src/util.cpp
@@ -148,6 +148,8 @@ namespace util {
 		return tokens;
 	}
 
+	std::map<std::string, std::string> remember_exec_paths;
+
 	/**
 	 * @brief Searches for an executable file with the specified name in the given list of directories.
 	 * @param command The name of the executable file to search for.
@@ -155,6 +157,11 @@ namespace util {
 	 */
 	std::string find_exec_in_path(std::string command)
 	{
+		if (remember_exec_paths.count(command) != 0)
+		{
+			return remember_exec_paths[command];
+		}
+
 		std::string exec_path = "";
 
 		// It will look through the path directories
@@ -178,10 +185,12 @@ namespace util {
 				{
 					// Store in memor
 					exec_path = entry.path().string();
-					return exec_path;
 				}
 			}
 		}
+
+		// Remember path for this command for future use
+		remember_exec_paths[command] = exec_path;
 
 		return exec_path;
 	}
